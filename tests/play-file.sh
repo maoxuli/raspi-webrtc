825415aac622a919d5ff93af233698d742f7e4c6
Maoxu Li <li@maoxuli.com>
2024-03-09 14:55:35 -0500

    using home location for url

--- tests/play-file.sh
+++ tests/play-file.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -x 
 
-gst-launch-1.0 -v filesrc location="/home/mli@novateur.com/Serenity-HD-DVD-Trailer.mp4" ! qtdemux ! decodebin ! videoconvert ! autovideosink 
+gst-launch-1.0 -v filesrc location="$HOME/Serenity-HD-DVD-Trailer.mp4" ! qtdemux ! decodebin ! videoconvert ! autovideosink 
--- tests/udp-send-file.sh
+++ tests/udp-send-file.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -x 
 
-gst-launch-1.0 -v filesrc location="/home/mli@novateur.com/Serenity-HD-DVD-Trailer.mp4" ! qtdemux ! h264parse config-interval=-1 ! rtph264pay ! udpsink port=5000 host=127.0.0.1
+gst-launch-1.0 -v filesrc location="$HOME/Serenity-HD-DVD-Trailer.mp4" ! qtdemux ! h264parse config-interval=-1 ! rtph264pay ! udpsink port=5000 host=127.0.0.1
