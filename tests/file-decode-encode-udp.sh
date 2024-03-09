e179d8214e889d3c9625a26bba388774c31e26c8
Maoxu Li <li@maoxuli.com>
2024-03-09 16:01:40 -0500

    host ip

--- tests/file-decode-encode-udp.sh
+++ tests/file-decode-encode-udp.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -x 
 
-# gst-launch-1.0 -v filesrc location="$HOME/Serenity-HD-DVD-Trailer.mp4" ! qtdemux ! h264parse config-interval=-1 ! omxh264dec ! x264enc ! rtph264pay ! udpsink port=5000 host=192.168.1.100
-gst-launch-1.0 -v filesrc location="$HOME/Serenity-HD-DVD-Trailer.mp4" ! qtdemux ! h264parse config-interval=-1 ! avdec_h264 ! x264enc ! rtph264pay ! udpsink port=5000 host=192.168.1.100
+# gst-launch-1.0 -v filesrc location="$HOME/Serenity-HD-DVD-Trailer.mp4" ! qtdemux ! h264parse config-interval=-1 ! omxh264dec ! x264enc ! rtph264pay ! udpsink port=5000 host=172.18.0.1
+gst-launch-1.0 -v filesrc location="$HOME/Serenity-HD-DVD-Trailer.mp4" ! qtdemux ! h264parse config-interval=-1 ! avdec_h264 ! x264enc ! rtph264pay ! udpsink port=5000 host=172.18.0.1
