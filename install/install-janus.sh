f83c70f5b437339edeccdddaf36c193eedf8913d
Maoxu Li <li@maoxuli.com>
2024-03-09 16:50:29 -0500

    install libsrtp2

--- install/install-janus.sh
+++ install/install-janus.sh
@@ -5,3 +5,14 @@ set -x
 git clone --depth 1 --branch 0.1.18 https://gitlab.freedesktop.org/libnice/libnice.git
 cd libnice 
 meson --prefix=/usr build && ninja -C build && sudo ninja -C build install
+cd ..
+rm -rf libnice 
+
+## libsrtp2 from source code 
+wget https://github.com/cisco/libsrtp/archive/v2.2.0.tar.gz
+tar xfv v2.2.0.tar.gz
+cd libsrtp-2.2.0
+./configure --prefix=/usr --enable-openssl
+make shared_library && sudo make install
+cd ..
+rm -rf v2.2.0.tar.gz libsrtp-2.2.0
