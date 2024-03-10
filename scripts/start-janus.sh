cf215d3776549268efb749f0a692d2d8e44395dd
Maoxu Li <li@maoxuli.com>
2024-03-09 20:49:48 -0500

    janus start script

--- scripts/start-janus.sh
+++ scripts/start-janus.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
 set -x 
+
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CONFIG_DIR="${SCRIPTS_DIR}/janus"
+
+janus -C "${CONFIG_DIR}/janus.jcfg" -F "${CONFIG_DIR}"
