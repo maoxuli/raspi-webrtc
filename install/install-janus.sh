411b7454a43b3030e1e60c301e3dd1d3c8c7eedf
Maoxu Li <li@maoxuli.com>
2024-03-09 20:57:08 -0500

    update janus install

--- install/install-janus.sh
+++ install/install-janus.sh
@@ -7,23 +7,27 @@ sudo apt-get update && apt-get install -y \
     automake \
     libtool \
     libconfig-dev \
+    libssl-dev \
     libjansson-dev \
-    libssl-dev
+    libmicrohttpd-dev \
+    libcurl4-openssl-dev \
+&& apt-get clean \
+&& rm -rf /var/lib/apt/lists/* 
 
-## libnice 0.1.18 from source code 
-git clone --depth 1 --branch 0.1.18 https://gitlab.freedesktop.org/libnice/libnice.git
-cd libnice 
-meson --prefix=/usr build && ninja -C build && sudo ninja -C build install
-cd ..
-rm -rf libnice 
+# ## libnice 0.1.18 from source code 
+# git clone --depth 1 --branch 0.1.18 https://gitlab.freedesktop.org/libnice/libnice.git
+# cd libnice 
+# meson --prefix=/usr build && ninja -C build && sudo ninja -C build install
+# cd ..
+# rm -rf libnice 
 
-## libsrtp2 from source code 
-wget https://github.com/cisco/libsrtp/archive/v2.2.0.tar.gz
-tar xfv v2.2.0.tar.gz
-cd libsrtp-2.2.0
-./configure --prefix=/usr --enable-openssl && make shared_library && sudo make install
-cd ..
-rm -rf v2.2.0.tar.gz libsrtp-2.2.0
+# ## libsrtp2 from source code 
+# wget https://github.com/cisco/libsrtp/archive/v2.2.0.tar.gz
+# tar xfv v2.2.0.tar.gz
+# cd libsrtp-2.2.0
+# ./configure --prefix=/usr --enable-openssl && make shared_library && sudo make install
+# cd ..
+# rm -rf v2.2.0.tar.gz libsrtp-2.2.0
 
 ## janus from source code 
 git clone --depth 1 --branch v1.2.1 https://github.com/meetecho/janus-gateway.git
