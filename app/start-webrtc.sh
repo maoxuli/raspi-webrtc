ac1fdda72e73f7a015f90fe07654b9bef2971e28
Maoxu Li <li@maoxuli.com>
2024-03-10 00:32:25 -0500

    update

--- app/start-webrtc.sh
+++ app/start-webrtc.sh
@@ -5,7 +5,8 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # start nginx service
 NGINX_CONFIG="${CURRENT_DIR}/nginx/nginx.conf"
-nginx -c "${NGINX_CONFIG}"
+NGINX_ROOT="${CURRENT_DIR}/nginx/www"
+nginx -c "${NGINX_CONFIG}" -p "${NGINX_ROOT}"
 
 # start janus
 JANUS_CONFIG="${CURRENT_DIR}/janus"
