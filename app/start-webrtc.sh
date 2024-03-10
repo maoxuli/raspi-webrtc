6a9787229c12a7f7d945df99f3cc7fc852535ee8
Maoxu Li <li@maoxuli.com>
2024-03-09 23:45:22 -0500

    bug

--- app/start-webrtc.sh
+++ app/start-webrtc.sh
@@ -3,11 +3,13 @@ set -x
 
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# restart nginx service
+# start nginx service
 NGINX_CONFIG="${CURRENT_DIR}/nginx/nginx.conf"
-sudo systemctl stop nginx.service 
-sudo nginx -c "${NGINX_CONFIG}"
+nginx -c "${NGINX_CONFIG}"
 
 # start janus
 JANUS_CONFIG="${CURRENT_DIR}/janus"
 janus -C "${JANUS_CONFIG}/janus.jcfg" -F "${JANUS_CONFIG}"
+
+# clean nginx service
+pkill -f nginx
