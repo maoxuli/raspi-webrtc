120d8b4cfbb6d7118f444223b725aa97f5b1eca1
Maoxu Li <li@maoxuli.com>
2024-03-09 23:00:26 -0500

    buf

---
+++ app/start-janus.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -x 
+
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+JANUS_CONFIG="${CURRENT_DIR}/janus"
+
+janus -C "${JANUS_CONFIG}/janus.jcfg" -F "${JANUS_CONFIG}"
--- app/start-nginx.sh
+++ app/start-nginx.sh
@@ -2,9 +2,6 @@
 set -x 
 
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-JANUS_CONFIG="${CURRENT_DIR}/janus"
-janus -C "${JANUS_CONFIG}/janus.jcfg" -F "${JANUS_CONFIG}"
-
 NGINX_CONFIG="${CURRENT_DIR}/web/nginx.conf"
+
 nginx -c "${NGINX_CONFIG}"
